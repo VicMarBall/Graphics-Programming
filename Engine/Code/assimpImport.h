89f3629affd9fdc925a735fd32323be01bb2ecda
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2025-03-27 10:30:30 +0100

    Update assimpImport.h

--- Engine/Code/assimpImport.h
+++ Engine/Code/assimpImport.h
@@ -1,7 +1,7 @@
 #include <assimp/cimport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-#include "engine.h"
+#include "resources.h"
 
 void ProcessAssimpMesh(const aiScene* scene, aiMesh* mesh, Mesh* myMesh, u32 baseMeshMaterialIndex, std::vector<u32>& submeshMaterialIndices)
 {
